22036f201651efb65fe5b21f01f710bdc0973fc9
dagbouj hatem <dagboujhatem@gmail.com>
2022-05-30 20:04:17 +0100

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Base path of all APIs
   app.setGlobalPrefix("api/v1");
+  // Swagger APIs documentation
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('NestJS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // End Swagger APIs documentation
   await app.listen(3000);
 }
 bootstrap();
