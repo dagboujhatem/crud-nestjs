d244cf614d2e9d9e022c34b195d10e82b6ad6c4a
dagbouj hatem <dagboujhatem@gmail.com>
2022-05-31 18:00:34 +0100

    connect to database and create the model

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ContactsModule } from './contacts/contacts.module';
 import { TodosModule } from './todos/todos.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, ContactsModule, TodosModule],
+  imports: [UsersModule, ContactsModule, TodosModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo, TodoDocument } from './entities/todo.entity';
 
 @Injectable()
 export class TodosService {
+
+  constructor(@InjectModel(Todo.name) private toDoModel: Model<TodoDocument>) {
+
+  }
+
   create(createTodoDto: CreateTodoDto) {
     return 'This action adds a new todo';
   }
 
-  findAll() {
-    return `This action returns all todos`;
+  async findAll(): Promise<Todo[]> {
+    return this.toDoModel.find();
   }
 
   findOne(id: number) {
